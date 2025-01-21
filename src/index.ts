209f300cda4a46bd4ba25d7a745a018f3992f9a0
神代綺凛 <i@loli.best>
2025-01-21 16:21:27 +0800

    Open when available

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import open from 'open';
 import puppeteer from 'puppeteer';
 
 const IS_PREPARE = false;
+const TARGET_URL =
+  'https://buy.cloud.tencent.com/lighthouse?blueprintType=PURE_OS&blueprintOfficialId=lhbp-mxml4cnq&regionId=5&zone=ap-hongkong-1';
 
 const browser = await puppeteer.launch({
   ...(IS_PREPARE
@@ -14,11 +17,9 @@ const browser = await puppeteer.launch({
 });
 const page = (await browser.pages())[0];
 
-await page.goto(
-  'https://buy.cloud.tencent.com/lighthouse?blueprintType=PURE_OS&blueprintOfficialId=lhbp-mxml4cnq&regionId=5&zone=ap-hongkong-1'
-);
+await page.goto(TARGET_URL);
 
-const isSoldOut = async (index: number) => {
+const getSoldOut = async (index: number) => {
   const soldOutList = await page.evaluate(() =>
     Array.from(document.querySelectorAll('.buy-compare-package-option')).map(
       (el) => el.classList.contains('is-soldout')
@@ -31,7 +32,11 @@ const isSoldOut = async (index: number) => {
 const check = async () => {
   await page.waitForSelector('.buy-compare-package-option .unit');
   console.log(new Date().toLocaleString());
-  console.log('2c2g sold out', await isSoldOut(1));
+  const isSoldOut = await getSoldOut(1);
+  console.log('2c2g sold out', isSoldOut);
+  if (!isSoldOut) {
+    await open(TARGET_URL);
+  }
 };
 
 check();
